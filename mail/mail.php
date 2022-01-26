2a3390fa6e82ba321c3230784a2eedd9ba19b093
yosmar puig <yosmarpuigs@gmail.com>
2022-01-26 02:10:05 +0100

    1.1

--- mail/mail.php
+++ mail/mail.php
@@ -11,7 +11,7 @@ Message: ".$message."
 
 ";
 
-mail ("name@youremail.com" , "New Message", $email_message);
+mail ("yosmarpuigs@gmail.com" , "New Message", $email_message);
 header("location: ../mail-success.html ");
 ?>
 
